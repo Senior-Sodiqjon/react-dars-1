3f33bc8a4493d4762a589415c942a30dc3724b9d
sodiqovsodiqjon <sodiqjon191929@gmail.com>
2023-09-01 19:04:09 +0500

    ob xavo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
+const apiKey = "3c55d79ad6bcde2f6f031ce795914e1c";
+
+function WeatherComponent() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Clean up the interval on component unmount
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    fetch(`${baseUrl}q=Toshkent&appid=${apiKey}`)
+      .then(response => response.json())
+      .then(data => setWeatherData(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  if (!weatherData) {
+    return <div>Loading...</div>;
+  }
+
+  const { main, weather, wind, name } = weatherData;
+  const { temp, humidity } = main;
+  const { speed, gust } = wind;
+  const { description } = weather[0];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="weather__box">
+        <div className="address__info">
+          <h1 className='addres__title'>{name}</h1>
+          {/* <b className='address__text address__one'>{description}</b> */}
+          <p className='address__text'>{currentTime.toLocaleTimeString()}</p>
+          <div id="txt"></div>
+        </div>
+        <div className="temprature">
+          <h1 className='temprature__title'>{Math.round(temp - 273.15)} ℃</h1>
+        </div>
+        <div className="wind__speed">
+          {/* <p className='wind__text'>Namlik: {humidity}%</p>
+          <p className='wind__text'>Shamol: {speed} m/s</p> */}
+          <p className='wind__text'>Yuqori tezlik: {gust} m/s</p>
+          <p className='wind__text'>Past tezlik: {gust} m/s</p>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default WeatherComponent;
